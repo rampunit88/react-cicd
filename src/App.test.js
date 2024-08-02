cf3ffe9be2d907aa22633630fa3e2921685b1ae9
Ram Punit <test.punit88@gmail.com>
2024-08-02 20:28:41 +0530

    Update main.yml

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/react/i);
   expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/welcome/i)).toBeInTheDocument();
 });
